23e26424bf5ade6a60e562aab52c02b1ec133ce9
Davi Bernardo <davi.bernardo@sbahq.org>
2022-12-28 17:02:58 -0300

    Painel admin parte 3

--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -2,11 +2,11 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-6">
-                <script>document.write(new Date().getFullYear())</script> © Upcube.
+                <script>document.write(new Date().getFullYear())</script> © Queimei a Mufa.
             </div>
             <div class="col-sm-6">
                 <div class="text-sm-end d-none d-sm-block">
-                    Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesdesign
+                    Criado com <i class="mdi mdi-heart text-danger"></i> por DNS DIGITAL TECH
                 </div>
             </div>
         </div>
